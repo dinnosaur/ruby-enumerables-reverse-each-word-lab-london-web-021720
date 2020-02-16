4890b8d9d545b0381891567b6b1f57f2c7f8893e
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-02-16 18:28:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,22 @@
 def reverse_each_word ( array)
 
-new_array=array.split
-array2 =[]
+# new_array=array.split
+# array2 =[]
+# 
+# new_array.each{|string|
+#   array2<<string.reverse
+# }
+# array3= array2.join(" ")
+# puts array3
+# return array3
+new_array = array.split 
+array2 = []
 
-new_array.each{|string|
-  array2<<string.reverse
+new_array.collect{|string|
+array2 << string reverse 
 }
-array3= array2.join(" ")
-puts array3
-return array3
+rray3= array2.join(" ")
+ puts array3
+ return array3
 
 end
