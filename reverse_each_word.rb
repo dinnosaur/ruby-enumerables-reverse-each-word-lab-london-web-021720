ebb65fcb1a86ceb8842e83f9d8757493a47fc4dd
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-02-16 18:28:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,18 +2,18 @@ def reverse_each_word ( array)
 
 # new_array=array.split
 # array2 =[]
-# 
+#
 # new_array.each{|string|
 #   array2<<string.reverse
 # }
 # array3= array2.join(" ")
 # puts array3
 # return array3
-new_array = array.split 
+new_array = array.split
 array2 = []
 
 new_array.collect{|string|
-array2 << string reverse 
+array2 << string reverse
 }
 rray3= array2.join(" ")
  puts array3
